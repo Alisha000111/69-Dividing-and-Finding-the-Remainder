dc9b84dc75bba9e8fcc77d1ac59b5fc4f566587c
Student <160208713+Alisha000111@users.noreply.github.com>
2024-03-24 14:01:29 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,7 @@
+
+function divideAndRemainder(dividend: number, divisor: number): { quotient: number; remainder: number } {
+    let quotient = Math.floor(dividend / divisor);
+    let remainder = dividend % divisor;
+    return { quotient, remainder };
+}
+console.log(divideAndRemainder(10, 3)); 
